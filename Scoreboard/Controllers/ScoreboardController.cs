f8c0818e2ea433ff0e49ad03da2b117815e921c0
Scott Hankinson <scott.hankinson@gmail.com>
2014-06-16 09:58:16 -0400

    Finalized USO data

--- Scoreboard/Controllers/ScoreboardController.cs
+++ Scoreboard/Controllers/ScoreboardController.cs
@@ -42,23 +42,23 @@ namespace Scoreboard.Controllers
         [Route("api/Scoreboard/usopen/2014")]
         public IHttpActionResult GetUSOpen2014()
         {
-            string serviceUrl = "http://www.usopen.com/";
-            string endpoint = "en_US/xml/gen/homeScores/homeScores.json";
-            RestClient client = new RestClient(serviceUrl);
-            RestRequest request = new RestRequest(endpoint, Method.GET);
-            request.AddHeader("Content-Type", "application/json");
-            request.RequestFormat = DataFormat.Json;
+            //string serviceUrl = "http://www.usopen.com/";
+            //string endpoint = "en_US/xml/gen/homeScores/homeScores.json";
+            //RestClient client = new RestClient(serviceUrl);
+            //RestRequest request = new RestRequest(endpoint, Method.GET);
+            //request.AddHeader("Content-Type", "application/json");
+            //request.RequestFormat = DataFormat.Json;
 
-            IRestResponse response = client.Execute(request);
+            //IRestResponse response = client.Execute(request);
 
-            return Ok(SimpleJson.DeserializeObject(response.Content.Replace("<!-- SSI Error -->", "")));
-            //string path = Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, @"App_Data\USOpen2014.json");
-            //string readContents;
-            //using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
-            //{
-            //    readContents = streamReader.ReadToEnd();
-            //}
-            //return Ok(SimpleJson.DeserializeObject(readContents));
+            //return Ok(SimpleJson.DeserializeObject(response.Content.Replace("<!-- SSI Error -->", "")));
+            string path = Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, @"App_Data\USOpen2014.json");
+            string readContents;
+            using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
+            {
+                readContents = streamReader.ReadToEnd();
+            }
+            return Ok(SimpleJson.DeserializeObject(readContents));
 
         }
 
