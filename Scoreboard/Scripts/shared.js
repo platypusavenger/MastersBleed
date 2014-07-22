3e06ce61bb25643052b3482f98b829600b1d8b0f
gtg489w <gtg489w@gmail.com>
2014-07-22 10:42:28 -0400

    Adding additional files for weather and shared.js

---
+++ Scoreboard/Scripts/shared.js
@@ -0,0 +1,13 @@
+﻿var getWeather = function () {
+    $.getJSON('https://api.forecast.io/forecast/4f5ddc4970c31afe753a70a6e3afc9c2/' + config.location.lat + ',' + config.location.lng + '?callback=?', function (data) {
+        console.log(data);
+        if (data.currently.precipIntensity > 0) {
+            $('body').css({ 'background-image': 'url(../images/' + config.backgrounds.raining + ')' });
+        }
+    });
+}
+$(function () {
+    $('body').css({ 'background-image': 'url(../images/' + config.backgrounds.default + ')' });
+    setInterval(getWeather, 1000 * 60 * 30); // every 30 minutes
+    getWeather();
+});
